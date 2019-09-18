29fac64a71acba420f66386e7d0b1945a18c9e2a
MRNIU <a6813140@hotmail.com>
2019-09-18 11:28:14 +0800

    working on max haep

--- DataStructuer/Heap.hpp
+++ DataStructuer/Heap.hpp
@@ -9,43 +9,99 @@
 #ifndef HEAP_HPP
 #define HEAP_HPP
 
-#include "AVLTree.hpp"
+//template <class T>
+//class HPN {
+//private:
+//    T data;
+//    HPN<T> * right;
+//    HPN<T> * left;
+//public:
+//    HPN(void);
+//    HPN(T data);
+//    ~HPN(void);
+//};
+//
+//template <class T>
+//HPN<T>::HPN(){
+//    return;
+//}
+//
+//template <class T>
+//HPN<T>::HPN(T data){
+//    return;
+//}
+//
+//template <class T>
+//HPN<T>::~HPN(){
+//    delete right;
+//    delete left;
+//    return;
+//}
 
+// Max Heap
 template <class T>
-class HPN {
+class Heap {
+private:
+//    HPN<T> * top;
+    T * ves;
+    size_t curr_size;
+    size_t max_size = 100;
 public:
-    T data;
-    HPN<T> * left;
-    HPN<T> * right;
-    HPN(void);
-    HPN(T data, HPN<T> * left = nullptr, HPN<T> * right = nullptr);
-    ~HPN(void);
+    Heap(void);
+    Heap(T * arr, size_t size);
+    ~Heap(void);
+    virtual const bool arry2heap(T * arr, size_t size);   // 数组转换为堆
+    virtual const bool push(T data);    // 插入元素
+    virtual const T pop(void);  // 弹出堆顶元素
+    virtual const T get_top(void);  // 获取堆顶元素
+    virtual const Heap<T> merge(Heap<T> * des, Heap<T> * res);   // 合并两个堆，返回新的堆
 };
 
 template <class T>
-HPN<T>::HPN(){
-    this->data = nullptr;
-    this->right = nullptr;
-    this->left = nullptr;
+Heap<T>::Heap(){
+    ves = (int *)malloc(max_size * sizeof(T));
     return;
 }
 
 template <class T>
-HPN<T>::HPN(T data, HPN<T> * left, HPN<T> * right){
-    this->data = data;
-    this->left = left;
-    this->right = right;
+Heap<T>::Heap(T * arr, size_t size){
+    ves = (int *)malloc(2 * size * sizeof(T));
+    for(int i = 0; i < size; i++){
+        ves[i] = arr[i];
+    }
     return;
 }
 
 template <class T>
-HPN<T>::~HPN(){
-    delete left;
-    delete right;
+Heap<T>::~Heap(){
+    delete ves;
     return;
 }
 
+template <class T>
+const bool Heap<T>::arry2heap(T * arr, size_t size){
+    return true;
+}
 
+template <class T>
+const bool Heap<T>::push(T data){
+    return true;
+}
+
+template <class T>
+const T Heap<T>::pop(){
+    return 0;
+}
+
+template <class T>
+const T Heap<T>::get_top(){
+    return 0;
+}
+
+template <class T>
+const Heap<T> Heap<T>::merge(Heap<T> * des, Heap<T> * res){
+    return Heap<T>();
+}
 
 
 #endif /* HEAP_HPP */
--- DataStructuer/main.cpp
+++ DataStructuer/main.cpp
@@ -24,39 +24,7 @@ clock_t startTime = clock(),
         endTime = 0;
 
 int main(int argc, const char * argv[]) {
-//    SPYTree<int> spyt(25);
-//    spyt.Insert(15);
-//    spyt.Insert(20);
-//    spyt.Insert(5);
-//    spyt.Insert(35);
-//    spyt.Insert(40);
-//    spyt.Insert(45);
-//    spyt.DisplayTree();
-//
-//
-//    spyt.Search_Splay(25);
-//    spyt.Search_Splay(15);
-//    spyt.Search_Splay(20);
-//    spyt.Search_Splay(5);
-//    spyt.Search_Splay(45);
-    
-
-    SPYTree<int> spyt(50);
-    spyt.Insert(40);
-    spyt.Insert(60);
-    spyt.Insert(30);
-    spyt.Insert(55);
-    spyt.Insert(70);
-    spyt.Insert(20);
-    spyt.DisplayTree();
-
-    spyt.Search_Splay(50);
-    spyt.Search_Splay(60);
-    spyt.Search_Splay(55);
-    spyt.Search_Splay(70);
-    spyt.Search_Splay(20);
-
-    spyt.DisplayTree();
+    Heap<int> heap;
     return 0;
 }
 
